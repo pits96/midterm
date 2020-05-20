5f82d21787dc5dd5b16002c0a15b5bd03d0f2ac1
Aaron Tadina <atadina@mail.greenriver.edu>
2020-05-20 18:03:21 -0400

    Added summary page

--- index.php
+++ index.php
@@ -13,9 +13,30 @@ $f3->route('GET /',function (){
    $view = new Template();
    echo $view->render('views/home.html');
 });
-$f3->route('GET /survey',function ($f3){
+$f3->route('GET|POST /survey',function ($f3){
     $view = new Template();
-    $f3->set('options',['Today is a good day.','Calm and collected','No need to rush']);
-    echo $view->render('views/survey.html');
+    $f3->set('options',['Today is a good day!','Calm and Collected','No need to rush']);
+    if($_SERVER['REQUEST_METHOD']=='POST') {
+        $_SESSION['name'] = $_POST['name'];
+        if(!empty($_POST['answers'])){
+//        foreach ($_POST['answers'] as $value){
+//            $choicestrng.=$value.", ";
+//        }
+            $choicestrng="";
+            for($i=0; $i<sizeof($_POST['answers'])-1;$i++){
+                $choicestrng.=$_POST["answers"][$i].", ";
+            }
+            $choicestrng.=$_POST['answers'][sizeof($_POST['answers'])-1];
+        }
+        $_SESSION['answersString'] = $choicestrng;
+        $f3->reroute('summary');
+    }
+        echo $view->render('views/survey.html');
+
+});
+$f3->route('GET|POST /summary',function (){
+    $view = new Template();
+    echo $view->render('views/summary.html');
+//    session_destroy();
 });
 $f3->run();
