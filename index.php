919b0da2ec767f1c887f837967832bc182246b30
Aaron Tadina <atadina@mail.greenriver.edu>
2020-05-20 17:07:53 -0400

    Added form page for the survey

--- index.php
+++ index.php
@@ -13,4 +13,9 @@ $f3->route('GET /',function (){
    $view = new Template();
    echo $view->render('views/home.html');
 });
+$f3->route('GET /survey',function ($f3){
+    $view = new Template();
+    $f3->set('options',['Today is a good day.','Calm and collected','No need to rush']);
+    echo $view->render('views/survey.html');
+});
 $f3->run();
